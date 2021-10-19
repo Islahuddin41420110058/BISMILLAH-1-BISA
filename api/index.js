5ca332adb1583201c1a6bdbb45f9af009d9805f3
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-10-19 18:20:21 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '2096759237:AAEETmtWsXDcQKJsYtSWGlC29S-XglZsgzs'
 const bot = new TelegramBot(token, {polling: true});
 
 
@@ -29,11 +29,11 @@ bot.onText(/\/menu/, (msg) => {
 });
 
 // routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
+r.get('/prediction/:S/:K', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
+            parseFloat(req.params.S), // string to float
+            parseFloat(req.params.K)
         ]
     ).then((jres)=>{
         res.json(jres);
