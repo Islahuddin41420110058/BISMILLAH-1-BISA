84f45da2ac891a3f13e989a1eebcbdbbd2e46a16
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-10-19 21:09:12 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -48,15 +48,15 @@ bot.on('message', (msg) => {
             cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
                 bot.sendMessage(
                         msg.chat.id,
-                        `nilai pompa yang diprediksi adalah ${jres1[0]} `
+                        `nilai pompa yang diprediksi adalah ${jres1[0]}`
                 })
                 bot.sendMessage(
                         msg.chat.id,
-                        `nilai pompa yang diprediksi adalah ${jres1[1]} `
+                        `nilai pompa yang diprediksi adalah ${jres1[1]}`
                 ); 
                 bot.sendMessage(
                         msg.chat.id,
-                        `Klasifikasi ${jres2} `
+                        `Klasifikasi ${jres2}`
                 );
                 state = 0;
             })
@@ -97,7 +97,7 @@ r.get('/classify/:S/:K', function(req, res, next) {
                 parseFloat(jres[1])
             ]
         ).then((jres_)=>{
-            res.json({jres, jres_});
+            res.json({jres, jres_})
         })
     })
 });
